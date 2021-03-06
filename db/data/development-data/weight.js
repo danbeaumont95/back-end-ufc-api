c31150946d7412cc5a995c1052b33038beadba05
Daniel <danielbeaumont95@hotmail.co.uk>
2021-03-06 20:22:41 +0000

    updating format of weights

--- db/data/development-data/weight.js
+++ db/data/development-data/weight.js
@@ -1,38 +1,38 @@
 module.exports = [
-    {
-        weights: 'Flyweight'
-    },
-    {
-        weights: 'Bantamweight'
-    },
-    {
-        weights: 'Featherweight'
-    },
-    {
-        weights: 'Lightweight'
-    },
-    {
-        weights: 'Welterweight'
-    },
-    {
-        weights: 'Middleweight'
-    },
-    {
-        weights: 'Light Heavyweight'
-    },
-    {
-        weights: 'Heavyweight'
-    },
-    {
-        weights: 'Womens Strawweight'
-    },
-    {
-        weights: 'Womens Flyweight'
-    },
-    {
-        weights: 'Womens Bantamweight'
-    },
-    {
-        weights: 'Womens Featherweight'
-    },
-]
+  {
+    weights: "Flyweight",
+  },
+  {
+    weights: "Bantamweight",
+  },
+  {
+    weights: "Featherweight",
+  },
+  {
+    weights: "Lightweight",
+  },
+  {
+    weights: "Welterweight",
+  },
+  {
+    weights: "Middleweight",
+  },
+  {
+    weights: "Light-Heavyweight",
+  },
+  {
+    weights: "Heavyweight",
+  },
+  {
+    weights: "Womens-Strawweight",
+  },
+  {
+    weights: "Womens-Flyweight",
+  },
+  {
+    weights: "Womens-Bantamweight",
+  },
+  {
+    weights: "Womens-Featherweight",
+  },
+];
